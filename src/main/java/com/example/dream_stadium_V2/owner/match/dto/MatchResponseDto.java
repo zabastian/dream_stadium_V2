e3bc9a0c237225422af6988e230dc66bc1db843b
zabastian <qlgh10gh@gmail.com>
2026-08-27 16:12:53 +0900

    :sparkles: 경기 생성 수정 조회 삭제 로직

--- src/main/java/com/example/dream_stadium_V2/owner/match/dto/MatchResponseDto.java
+++ src/main/java/com/example/dream_stadium_V2/owner/match/dto/MatchResponseDto.java
@@ -1,30 +1,39 @@
 package com.example.dream_stadium_V2.owner.match.dto;
 
 import com.example.dream_stadium_V2.owner.match.entity.Match;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@JsonPropertyOrder({
+        "matchId",
+        "cost",
+        "matchDate",
+        "homeTeamId",
+        "awayTeamId",
+        "userId",
+        "stadiumId"
+})
 public class MatchResponseDto {
 
     private Long matchId;
     private Long cost;
     private LocalDateTime matchDate;
-    private String homeTeam;
-    private String awayTeam;
+    private Long homeTeamId;
+    private Long awayTeamId;
     private Long userId;
     private Long stadiumId;
-    private Long teamId;
 
     public MatchResponseDto(Match match) {
         this.matchId = match.getId();
         this.cost = match.getCost();
         this.matchDate = match.getMatchDate();
-        this.homeTeam = match.getHomeTeam();
-        this.awayTeam = match.getAwayTeam();
+        this.homeTeamId = match.getHomeTeam().getId();
+        this.awayTeamId = match.getAwayTeam().getId();
         this.userId = match.getUser().getId();
         this.stadiumId = match.getStadium().getId();
-        this.teamId = match.getTeam().getId();
 
     }
-
 }
--- src/main/java/com/example/dream_stadium_V2/owner/match/repository/MatchRepository.java
+++ src/main/java/com/example/dream_stadium_V2/owner/match/repository/MatchRepository.java
@@ -1,9 +1,14 @@
 package com.example.dream_stadium_V2.owner.match.repository;
 
+import com.example.dream_stadium_V2.common.user.entity.User;
 import com.example.dream_stadium_V2.owner.match.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findByUser(User user);
+
 }
